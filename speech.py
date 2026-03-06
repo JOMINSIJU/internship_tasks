a1a32c73c0d84a70d0c46d4fce747db2624451bf
JOMIN S GEORGE <rosmysiju182@gmail.com>
2026-03-06 23:10:02 +0530

    Add files via upload

---
+++ speech.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Speak something...")
+    audio = r.listen(source)
+
+try:
+    text = r.recognize_google(audio)
+    print("You said:", text)
+
+except sr.UnknownValueError:
+    print("Could not understand audio")
+
+except sr.RequestError:
+    print("Could not request results")
